2f11d478cc93cb450afd42c6fb2b2549c73a4fb9
RuslanMirov <ruslan777mirov@gmail.com>
2021-10-13 16:11:45 +0300

    Update addresses

--- src/constants/address/address.js
+++ src/constants/address/address.js
@@ -10,10 +10,10 @@
 
 // TESTNET
 
-export const AddressDepositor='0x893E2f21AB55b97e92a2678E8aE76E247BD37010'
-export const ClaimableAddress='0xBC8EcCEd821605c69e6FF33E94c37b0905deefB8'
+export const AddressDepositor=''
+export const ClaimableAddress=''
 export const DEXFormulaAddress='0xe1b9BAE4c6D65454b6905333741d75bb1d1FCbe5'
 export const RouterAddress='0x666CAe17452Cf2112eF1479943099320AFD16d47'
 export const UNDERLYING_TOKEN ='0x42c5f95e15eE6a236722248Daf47909a3561d88e'
 export const BUSDAddress='0x62d915Db618Cf0F514a631d66be166CD1fCeE902'
-export const UNDERLYING_Pool='0xF090F82521957076C3808b42c4D9743787Ef7554'
+export const UNDERLYING_Pool=''
